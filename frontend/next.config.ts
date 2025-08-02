896ccdc9f8295c24fe0124a4ae5a3d867e3c2c59
Gabriel Seet <gabeseet@gmail.com>
2025-08-02 13:33:51 +0800

    build rules

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    // This is not recommended unless you already have ESLint configured to run
+    // in a separate part of your workflow (e.g., in CI or a pre-commit hook).
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
